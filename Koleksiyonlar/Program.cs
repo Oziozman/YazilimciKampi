31c005d76ac22ebf9e71937225f9f15012604e09
oguzhanbilgic7@outlook.com <oguzhanbilgic7@outlook.com>
2021-01-21 16:57:47 +0300

    Add project files.

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> isimler = new List<string>{"Ahmet", "Mehmet", "Ferhat", "Ali"};
+            Console.WriteLine(isimler[0]);
+            isimler.Add("Mahmut");
+
+        }
+    }
+}
